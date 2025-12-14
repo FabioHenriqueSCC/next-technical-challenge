008c7045831080bf1ceb2297789457011de63374
FabioHenriqueSCC <fhscc7@gmail.com>
2025-12-14 12:00:40 -0300

    fix: problems related in next build

--- frontend/src/app/zones/page.tsx
+++ frontend/src/app/zones/page.tsx
@@ -1,4 +1,11 @@
-import ZonesPage from '@/src/features/zones/components/ZonesPage';
+'use client';
+
+import dynamic from 'next/dynamic';
+
+const ZonesPage = dynamic(
+  () => import('@/src/features/zones/components/ZonesPage'),
+  { ssr: false },
+);
 
 export default function Page() {
   return <ZonesPage />;
